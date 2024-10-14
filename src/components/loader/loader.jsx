2b1e9f4a240181f55837d5b5fa1e29d9d272f11a
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-10-14 20:42:46 +0300

    Updates

--- src/components/loader/loader.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import style from "./loader.module.css";
-
-export default function Loader() {
-  return <div className={style.loadSpinBox}></div>;
-}
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Main from "./Main";
-import List from "./List";
-import Cards from "./Cards";
-import Error404 from "./Error404";
-
-export { Main, List, Cards, Error404 };
